83f885d850e3d2a431ae8fadaf009826645a7273
Vlad Gurovich <vlad.gurovich@gmail.com>
2012-09-08 11:40:57 -0700

    updated token length

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DisruptTemplate::Application.config.secret_token = 'SUPER_SEKRET_TOKEN'
+DisruptTemplate::Application.config.secret_token = 'SUPER_SEKRET_TOKENSUPER_SEKRET_TOKENSUPER_SEKRET_TOKENSUPER_SEKRET_TOKEN'
